10a081cdb6e6c891366e777419decc503b38eb58
François Voron <fvoron@kpi-intelligence.com>
2019-12-18 16:29:26 +0100

    Update debug script

--- debug.py
+++ debug.py
@@ -11,40 +11,13 @@ async def open_connection(host: str, ssl: bool = True) -> (asyncio.StreamReader,
     return reader, writer
 
 
-async def make_get(host: str, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
-    query = (
-        f"GET / HTTP/1.1\r\n"
-        f"Host: {host}\r\n"
-        f"\r\n"
-    )
-
-    writer.write(query.encode('latin-1'))
-    while True:
-        line = await reader.readline()
-        if not line:
-            break
-
-        line = line.decode('latin1').rstrip()
-        if line:
-            logger.debug(f'HTTP header> {line}')
-
-
-async def close(writer: asyncio.StreamWriter) -> None:
-    print("Done?", writer._protocol._closed.done())
-    writer.close()
-    print("Done?", writer._protocol._closed.done())
-    await writer.wait_closed()
-    print("Done?", writer._protocol._closed.done())
-    # Will be stuck here
-
-
 async def main(host: str):
     reader, writer = await open_connection(host)
     logger.debug(f"Connection to {host} opened")
-    #await make_get(host, reader, writer)
 
     logger.debug(f"Close stream writer...")
-    await close(writer)
+    writer.close()
+    await writer.wait_closed()
 
 
 asyncio.run(main("login.microsoftonline.com"), debug=True)
