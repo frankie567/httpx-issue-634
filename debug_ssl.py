a48b3406de62a42d3dd977859222a8abcbb3dd3a
François Voron <fvoron@kpi-intelligence.com>
2019-12-18 17:26:57 +0100

    Update debug_ssl script

--- debug_ssl.py
+++ debug_ssl.py
@@ -1,12 +1,10 @@
 import socket
 import ssl
 
-hostname = 'login.microsoftonline.com'
+hostname = 'www.facebook.com'
 context = ssl.create_default_context()
 
 with socket.create_connection((hostname, 443)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         ssock.unwrap()
-        d = ssock.recv(1024 * 256)
-        print(d)
         ssock.close()
